2590102bef9434aa7fa956f5eaa56b2872620c05
JGrant024 <jonathan.grant23@icloud.com>
2024-02-13 22:48:54 -0500

    updated exercises

--- main.py
+++ main.py
@@ -9,12 +9,23 @@ async def root():
 
 # Step 2 - New endpoint Make a get endpoint and call it items
 @app.get("/items/")
-async def display_items(skip: int = 0, limit: int = 10): 
-    display_items = [
-        {"id": 1, "name": "Jonathan "},
+async def display_items(skip: int = 0, limit: int = 10, q: str = None): 
+    if q:
+        response_data = {"q": q, "skip": skip, "limit": limit}
+    else:
+        response_data = {"skip": skip, "limit": limit}
+        
+    items = [
+        {"id": 1, "name": "Jonathan"},
         {"id": 2, "name": "Grant"}
     ]
-    return {"skip": skip, "limit": limit, "items": display_items}
+    
+    if q:
+        items = [item for item in items if q.lower() in item["name"].lower()]
+        response_data["items"] = items
+        
+    return response_data
+    
 
 
 # Step 3- Have the new endpoint accept a path parameter called `item_id
@@ -22,5 +33,13 @@ async def display_items(skip: int = 0, limit: int = 10):
 @app.get("/items/{item_id}") 
 async def display_items(item_id: int):
     return{"item_id": item_id}
-   
-   
+
+# Step 4
+@app.get("/users/{user_id}")
+async def display_user(user_id: int):
+    return {"user_id": user_id}
+
+# step 5
+@app.get("/users/{user_id}/items/")
+async def display_user_items(user_id: int, skip: int = 0, limit: int = 10):
+    return {"user_id": user_id, "skip": skip, "limit": limit}
